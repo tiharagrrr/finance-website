d7344c2acc83cbcafa8d33e43a7c91d06529fd84
Tihara Egodage <tiharaeg@gmail.com>
2024-06-04 14:30:25 +0530

    fixed navbar bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,10 +21,10 @@ const Navbar = () => {
         <li className='p-4'>Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden cursor-pointer' >
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         
       </div>
-      <div className={!nav ? 'md:hidden fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 ' :'md:hidden fixed left-[-100%]' }>
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 ' :'md:hidden fixed left-[-100%]' }>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>React</h1>
         <ul className='p-4 uppercase' >
           <li className='p-4 border-gray-600 border-b'>Home</li>
